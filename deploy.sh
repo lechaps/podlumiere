1fcc78559c6d539ac623eacb0820caf0960dabe9
lechaps <33101610+lechaps@users.noreply.github.com>
2019-12-21 01:04:14 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,7 @@ cd public
 tar -cf Website.tar  *   
 
 curl -T Website.tar -u $FTP_USER:$FTP_PASS $FTP_HOST
+
+ssh $SSH_CONNECTION -p 5022 
+
+cd public_html && tar -xvf Website.tar && rm Website.tar
