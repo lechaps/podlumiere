a5d2fc64843c34e9a66c807114dd739430081e23
lechaps <33101610+lechaps@users.noreply.github.com>
2019-12-21 02:50:47 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 # deploy.sh
 
 
-lftp ftp://$FTP_USER:$FTP_USER@$FTP_HOST -e "set ssl:verify-certificate no;mirror -e -R public / ; quit"
+lftp ftp://$FTP_USER:$FTP_PASS@$FTP_HOST -e "set ssl:verify-certificate no;mirror -e -R public / ; quit"
