b962ddeefd3cf9b7b44b25ffd15f419b734e225f
lechaps <33101610+lechaps@users.noreply.github.com>
2019-12-21 02:21:12 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 # deploy.sh
 
-# Tar Website
-cd public 
-tar -cf Website.tar  *   
 
-# Transfert
-curl -T Website.tar -u $FTP_USER:$FTP_PASS $FTP_HOST
-
-#Connection & deTar & delete
-ssh $SSH_CONNECTION -p 5022 
-cd public_html && tar -xvf Website.tar && rm Website.tar
+lftp ftp://$FTP_USER:$FTP_USER@$FTP_HOST -e "set ssl:verify-certificate no;mirror -e -R public / ; quit"
